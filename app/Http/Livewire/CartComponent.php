e35b85b255df585865abbf80ecff9dabe2fbf1c4
Chandan-Ojha <chandanojha776@gmail.com>
2021-11-13 20:16:27 +0600

    create & apply coupons done

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -2,77 +2,132 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Coupon;
 use Livewire\Component;
+use Carbon\Carbon;
 use Cart;
 class CartComponent extends Component
 {
-       //increase cart quantity
-       public function increaseQuantity($rowId)
-        {
-           $product = Cart::instance('cart')->get($rowId);
-           $qty = $product->qty + 1;
-           Cart::instance('cart')->update($rowId, $qty);
-           $this->emitTo('cart-count-component', 'refreshComponent');
-        }
+    public $haveCouponCode;
+    public $couponCode;
+    public $discount;
+    public $subtotalAfterDiscount;
+    public $taxAfterDiscount;
+    public $totalAfterDiscount;
 
-        //decrease cart quantity
-        public function decreaseQuantity($rowId)
-        {
-          $product = Cart::instance('cart')->get($rowId);
-          $qty = $product->qty - 1;
-          Cart::instance('cart')->update($rowId, $qty);
-          $this->emitTo('cart-count-component', 'refreshComponent');
-        }
+    //increase cart quantity
+    public function increaseQuantity($rowId)
+    {
+      $product = Cart::instance('cart')->get($rowId);
+      $qty = $product->qty + 1;
+      Cart::instance('cart')->update($rowId, $qty);
+      $this->emitTo('cart-count-component', 'refreshComponent');
+    }
 
-        //remove product from cart 
-        public function destroy($rowId)
-        {
-          Cart::instance('cart')->remove($rowId);
-          $this->emitTo('cart-count-component', 'refreshComponent');
-          session()->flash('success_message', 'Item has been removed');
+    //decrease cart quantity
+    public function decreaseQuantity($rowId)
+    {
+      $product = Cart::instance('cart')->get($rowId);
+      $qty = $product->qty - 1;
+      Cart::instance('cart')->update($rowId, $qty);
+      $this->emitTo('cart-count-component', 'refreshComponent');
+    }
+
+    //remove product from cart 
+    public function destroy($rowId)
+    {
+      Cart::instance('cart')->remove($rowId);
+      $this->emitTo('cart-count-component', 'refreshComponent');
+      session()->flash('success_message', 'Item has been removed');
         
-        }
+    }
 
-        //remove all product from cart 
-        public function destroyAll()
-        {
-           Cart::instance('cart')->destroy();
-           $this->emitTo('cart-count-component', 'refreshComponent');
+    //remove all product from cart 
+    public function destroyAll()
+    {
+      Cart::instance('cart')->destroy();
+      $this->emitTo('cart-count-component', 'refreshComponent');
         
-        }
+    }
 
-        //Add product to SaveforLater
-        public function switchToSaveForLater($rowId)
-        {
-            $item = Cart::instance('cart')->get($rowId);
-            Cart::instance('cart')->remove($rowId);
-            Cart::instance('saveForLater')->add($item->id, $item->name, 1, $item->price)->associate('App\Models\Product');
-            $this->emitTo('cart-count-component', 'refreshComponent');
-            session()->flash('success_message', 'Item has been saved for later');
-        }
+    //Add product to SaveforLater
+    public function switchToSaveForLater($rowId)
+    {
+      $item = Cart::instance('cart')->get($rowId);
+      Cart::instance('cart')->remove($rowId);
+      Cart::instance('saveForLater')->add($item->id, $item->name, 1, $item->price)->associate('App\Models\Product');
+      $this->emitTo('cart-count-component', 'refreshComponent');
+      session()->flash('success_message', 'Item has been saved for later');
+    }
 
-        //Move product from SaveforLater to cart
-        public function moveToCart($rowId)
-        {
-            $item = Cart::instance('saveForLater')->get($rowId);
-            Cart::instance('saveForLater')->remove($rowId);
-            Cart::instance('cart')->add($item->id, $item->name, 1, $item->price)->associate('App\Models\Product');
-            $this->emitTo('cart-count-component', 'refreshComponent');
-            session()->flash('s_success_message', 'Item has been moved to cart');
-        }
+    //Move product from SaveforLater to cart
+    public function moveToCart($rowId)
+    {
+      $item = Cart::instance('saveForLater')->get($rowId);
+      Cart::instance('saveForLater')->remove($rowId);
+      Cart::instance('cart')->add($item->id, $item->name, 1, $item->price)->associate('App\Models\Product');
+      $this->emitTo('cart-count-component', 'refreshComponent');
+      session()->flash('s_success_message', 'Item has been moved to cart');
+    }
     
-        //delete product from SaveforLater
-        public function deleteFromSaveForLater($rowId)
-        {
-            Cart::instance('saveForLater')->remove($rowId);
-            session()->flash('s_success_message', 'Item has been removed from save for later');
-        }
+    //delete product from SaveforLater
+    public function deleteFromSaveForLater($rowId)
+    {
+      Cart::instance('saveForLater')->remove($rowId);
+      session()->flash('s_success_message', 'Item has been removed from save for later');
+    }
 
-    
+   //Apply Coupon Code
+   public function applyCouponCode()
+   {
+       $coupon = Coupon::where('code', $this->couponCode)->where('expiry_date', '>=', Carbon::today())->where('cart_value', '<=', Cart::instance('cart')->subtotal())->first();
+       if (!$coupon) {
+           session()->flash('coupon_message', 'Coupon code is invalid');
+           return;
+       }
+     session()->put('coupon', [
+           'code' => $coupon->code,
+           'type' => $coupon->type,
+           'value' => $coupon->value,
+           'cart_value' => $coupon->cart_value
+       ]);
+   }
 
-        public function render()
-        {
-          return view('livewire.cart-component')->layout('layouts.base');
+    //Discount Calculation
+    public function calculateDiscounts()
+    {
+        if (session()->has('coupon')) {
+            if (session()->get('coupon')['type'] == 'fixed') {
+                $this->discount = session()->get('coupon')['value'];
+            } else {
+                $this->discount = (Cart::instance('cart')->subtotal() * session()->get('coupon')['value']) / 100;
+            }
+            $this->subtotalAfterDiscount = Cart::instance('cart')->subtotal() - $this->discount;
+            $this->taxAfterDiscount = ($this->subtotalAfterDiscount * config('cart.tax')) / 100;
+            $this->totalAfterDiscount = $this->subtotalAfterDiscount + $this->taxAfterDiscount;
         }
+    }
+
+    //Remove Coupon
+    public function removeCoupon()
+    {
+        session()->forget('coupon');
+    }
+
+
+
+    public function render()
+    {
+      if (session()->has('coupon')) {
+        if (Cart::instance('cart')->subtotal() < session()->get('coupon')['cart_value']) {
+            session()->forget('coupon');
+        } else {
+            $this->calculateDiscounts();
+        }
+      }
+      
+      return view('livewire.cart-component')->layout('layouts.base');
+    }
+
 }
 
