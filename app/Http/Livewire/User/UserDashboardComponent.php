4a6f5bf5c35a155de442e6933ff723d3bf2e17f9
Chandan-Ojha <chandanojha776@gmail.com>
2021-11-19 15:43:12 +0600

    display data on user dashboard

--- app/Http/Livewire/User/UserDashboardComponent.php
+++ app/Http/Livewire/User/UserDashboardComponent.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Livewire\User;
 
+use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class UserDashboardComponent extends Component
 {
     public function render()
     {
-        return view('livewire.user.user-dashboard-component')->layout('layouts.base');
+        $orders = Order::orderBy('created_at','DESC')->where('user_id',Auth::user()->id)->get()->take(10);
+        $totalCost = Order::where('status','!=','canceled')->where('user_id',Auth::user()->id)->sum('total');
+        $totalPurchase = Order::where('status','!=','canceled')->where('user_id',Auth::user()->id)->count();
+        $totalDeliverd = Order::where('status','delivered')->where('user_id',Auth::user()->id)->count();
+        $totalCanceled = Order::where('status','canceled')->where('user_id',Auth::user()->id)->count();
+        return view('livewire.user.user-dashboard-component',[
+            'orders'=>$orders,
+            'totalCost'=>$totalCost,
+            'totalPurchase'=>$totalPurchase,
+            'totalDeliverd'=>$totalDeliverd,
+            'totalCanceled'=>$totalCanceled 
+            ])->layout('layouts.base');
     }
 }
