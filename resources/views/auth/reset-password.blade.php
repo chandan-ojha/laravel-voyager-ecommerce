c828a838c82f13a00292287767d576bc17ede1bf
Chandan-Ojha <chandanojha776@gmail.com>
2021-11-21 10:55:54 +0600

    forgot password done

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
@@ -33,4 +33,58 @@
             </div>
         </form>
     </x-jet-authentication-card>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+
+{{--Customize Password Reset form --}}
+
+<x-base-layout>
+    <main id="main" class="main-site left-sidebar">
+
+        <div class="container">
+
+            <div class="wrap-breadcrumb">
+                <ul>
+                    <li class="item-link"><a href="/" class="link">home</a></li>
+                    <li class="item-link"><span>Reset Password</span></li>
+                </ul>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12 col-md-offset-3">
+                    <div class=" main-content-area">
+                        <div class="wrap-login-item ">
+                            <div class="login-form form-item form-stl">
+                                <x-jet-validation-errors class="mb-4" />
+                                <form name="frm-login" method="POST" action="{{ route('password.update')}}">
+                                    @csrf
+                                    <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                                    <fieldset class="wrap-title">
+                                        <h3 class="form-title">Reset Password</h3>
+                                    </fieldset>
+                                    <fieldset class="wrap-input">
+                                        <label for="frm-login-uname">Email Address:</label>
+                                        <input type="email" id="frm-login-uname" name="email" placeholder="Type your email address" value="{{$request->email}}" required autofocus>
+                                    </fieldset>
+                                    <fieldset class="wrap-input item-width-in-half left-item ">
+                                        <label for="password">Password *</label>
+                                        <input type="password" id="password" name="password" placeholder="Password" required autocomplete="new-password">
+                                    </fieldset>
+                                    <fieldset class="wrap-input item-width-in-half ">
+                                        <label for="password_confirmation">Confirm Password *</label>
+                                        <input type="password" id="password_confirmation" name="password_confirmation" placeholder="Confirm Password" required autocomplete="new-password">
+                                    </fieldset>
+                                    <input type="submit" class="btn btn-submit" value="Reset Password" name="submit">
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end main products area-->
+                </div>
+            </div>
+            <!--end row-->
+
+        </div>
+        <!--end container-->
+
+    </main>
+</x-base-layout>
